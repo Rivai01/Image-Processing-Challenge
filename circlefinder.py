d4fc5c4f42385abd9417a32fb15ab79928e97da0
Levi Hauser <lah279@cornell.edu>
2026-03-18 12:49:29 -0400

    Add quadratic regression

--- circlefinder.py
+++ circlefinder.py
@@ -17,24 +17,6 @@ class CircleFinder:
         self.img = img                      # set image
         self.center = self.get_center()     # find the image's center
         self._init_radial_profile()         # find radial patterns
-    
-    def get_center(self):
-        """
-        Returns the coordinates of the center of `self.img`.
-        
-        Currently finds the brightest pixel, which is susceptible to noise and contamination, and
-        specific to the Metrology Algorithms Challenge images.
-        """
-        if 'center' in self.__dict__:       # check if the center has already been initialized
-            return self.center
-        else:                               # initialize the center
-            # Take median blur to counteract blotchy contamination
-            dst = cv.medianBlur(self.img, 39)
-            # Remove background larger than central blur to leave just central Gaussian
-            bg = cv.blur(dst, (301, 301))
-            dst_sub = cv.subtract(dst, bg)
-            # Find brightest point on central Gaussian
-            return cv.minMaxLoc(dst_sub)[3]
         
     def get_img(self):
         """Returns the image being analyzed."""
@@ -64,39 +46,69 @@ class CircleFinder:
         """
         return self.avg_rem_outliers
     
-
-    def get_ring_pixels(self, radius, width=1):
+    def get_center(self):
         """
-        Returns a 1D array containing the pixels at radius ``radius`` from ``self.center``.
-
-        :param radius: The (major) radius at which to select pixels
-        :param width: The minor radius of the annulus from which to select pixels around ``radius``
+        Returns the coordinates of the center of `self.img`.
+        
+        Currently finds the brightest pixel, which is susceptible to noise and contamination, and
+        specific to the Metrology Algorithms Challenge images.
         """
-        mask = np.abs(self.map - radius) <= width / 2
-        return self.img[mask]
+        if 'center' in self.__dict__:       # check if already initialized
+            return self.center
+        
+        else:                               # initialize
+            # Take median blur to counteract blotchy contamination
+            dst = cv.medianBlur(self.img, 39)
+
+            # Remove background larger than central blur to leave just central Gaussian
+            bg = cv.blur(dst, (301, 301))
+            dst_sub = cv.subtract(dst, bg)
+
+            # Find brightest point on central Gaussian
+            return cv.minMaxLoc(dst_sub)[3]
     
     def get_circles(self):
         """Returns an array containing the radii (in pixels) of the detected circles."""
-        if 'radii' in self.__dict__:
+        if 'radii' in self.__dict__:        # check if already initialized
             return self.radii 
-        else:
-            # typical smallest separation between circles is around 20 pixels
+        
+        else:                               # initialize
+            # find radii of minimum brightness
+            # distance = 20 and prominence = 3 are tuned parameters for this specific problem
             self.radii, _ = find_peaks(-self.avg_rem_outliers, distance=20, prominence=3)
-            if self.radii[0] < 10: # don't count the center point if it somehow gets detected
+
+            # don't count fringes at the center point if they get detected
+            if self.radii[0] < 10:
                 self.radii = self.radii[1:]
-            while self.radii[-1] > len(self.avg_rem_outliers) - 35: # Too little data to reliably detect at farmost corner
+
+            # don't count fringes near the farmost corner; too little data to reliably detect
+            while self.radii[-1] > len(self.avg_rem_outliers) - 25: 
                 self.radii = self.radii[:-1]
-            # self.radii = argrelmin(self.avg_rem_outliers, order=20)[0] 
+
             return self.radii
     
     def marked_img(self):
         """Returns a color image with the center and concentric circles marked."""
         new_img = cv.cvtColor(self.img, cv.COLOR_GRAY2BGR)
+
+        # draw fringes
         radii = self.get_circles()
         for radius in radii:
-            cv.circle(new_img, self.center, radius, (0, 255, 0), 3)
-        cv.circle(new_img, self.center, 10, (255, 0, 0), -1)
+            cv.circle(new_img, self.get_center(), radius, (0, 255, 0), 3)
+
+        # draw center
+        cv.circle(new_img, self.get_center(), 10, (255, 0, 0), -1)
+
         return new_img
+    
+    def quad_regression(self):
+        """
+        Run a quadratic regression on the fringe radii, and return [`c`, `b`, `a`],
+        where the modeled radius `r` at fringe number `n` is `r = an^2 + bn + c`.
+        """
+        n = np.arange(len(self.get_circles()))
+        coef = np.polynomial.polynomial.polyfit(n, self.get_circles(), deg=2)
+        return coef
 
     def _init_radial_profile(self):
         """
@@ -104,30 +116,39 @@ class CircleFinder:
         """
         h, w = self.img.shape
         y, x = np.ogrid[:h, :w]
-        cx, cy = self.center
+        cx, cy = self.get_center()
         xdist = x - cx
         ydist = y - cy
-        # radii in pixels
+
+        # calculate radii (in pixels) of each point in the image
         r = np.sqrt(xdist * xdist + ydist * ydist).astype(np.int32)
 
+        # blur to remove Gaussian noise
+        blurred = cv.blur(self.img, (10, 10))
+
+        # flatten for bincount
         r_flat = r.ravel()
-        # Take median blur to counteract blotchy contamination
-        img_flat = self.img.ravel().astype(np.int32)
+        img_flat = blurred.ravel().astype(np.int32)
+
+        # sort pixels by radii
         _, inv, counts = np.unique(r_flat, return_inverse=True, return_counts=True)
         # sum of values per radius
         sum_vals = np.bincount(inv, weights=img_flat)
         # sum of squares per radius
         sum_sq = np.bincount(inv, weights=np.square(img_flat))
+
+        # compute average and standard deviation
         avg = sum_vals / counts
         stdev = np.sqrt(np.maximum(0.0, sum_sq / counts - np.square(avg)))
 
-        # compute average without outliers (to filter out contamination)
+        # recompute average without outliers (to filter out blotchy contamination)
         dev = np.abs(img_flat - avg[inv])
         mask = dev < stdev[inv]
         sum_vals_noout = np.bincount(inv[mask], weights=img_flat[mask], minlength=len(counts))
         counts_noout = np.bincount(inv[mask], minlength=len(counts))
         avg_noout = np.divide(sum_vals_noout, counts_noout, where=counts_noout > 0)
 
+        # set object attributes
         self.avg = avg
         self.stdev = stdev
         self.avg_rem_outliers = avg_noout
--- main.py
+++ main.py
@@ -3,28 +3,42 @@ import matplotlib.pyplot as plt
 from circlefinder import CircleFinder
 import time
 import cv2 as cv
+import numpy as np
 
 if __name__ == "__main__":
-    img, params = generate_image(n_contam=5, noise_mag=10)
+    img, params = generate_image(n_contam=50, noise_mag=100)
     print("Seed: ", params.get("seed"))
+
+    # time algorithm
     start = time.time()
+
     finder = CircleFinder(img)
     circles = finder.get_circles()
+    coef = finder.quad_regression()
+
     end = time.time()
+    print(f"Regression: r = {coef[2]:.1f}n^2 + {coef[1]:.1f}n + {coef[0]:.1f}")
     print(f"Algorithm run time: {end - start:.4f} seconds")
+    
     marked_img = finder.marked_img()
     fig, ax = plt.subplots(1, 3, figsize=(15, 5))
     ax[0].imshow(cv.cvtColor(img, cv.COLOR_GRAY2BGR), cmap='gray')
     ax[0].set_title('Raw image')
+
     ax[1].imshow(marked_img, cmap='gray')
     ax[1].set_title('Annotated image')
-    ax[2].plot(circles, 'o')
+
+    ax[2].plot(circles, 'o', label='Detected')
+
+    smooth_n = np.linspace(0, len(circles), 100)
+    reg = coef[0] + coef[1] * smooth_n + coef[2] * np.square(smooth_n)
+    ax[2].plot(smooth_n, reg, label='Regression')
+    ax[2].legend()
+
     ax[2].set_title('Circle radii')
     ax[2].set_xlabel('Circle number')
     ax[2].set_ylabel('Radius (pixels)')
-    # ax[3].plot(finder.get_avg(), label='average')
-    # ax[3].plot(finder.get_avg_rem_outliers(), label='average w/o outliers')
-    # ax[3].legend()
-    # ax[3].set_title('Radial pixel values')
+
     fig.suptitle('Seed = ' + str(params.get("seed")))
+
     plt.show()
